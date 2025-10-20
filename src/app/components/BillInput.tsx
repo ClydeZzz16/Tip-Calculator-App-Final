7f9ed14b088e3825134610e3d22b2b0d7fb3ae19
Jean Clyde C. Egmilan <clydeskieegmilan@gmail.com>
2025-10-20 08:02:12 +0800

    Adds icons to input fields for enhanced UX

--- src/app/components/BillInput.tsx
+++ src/app/components/BillInput.tsx
@@ -1,6 +1,7 @@
 'use client';
-
 import React from 'react';
+import Image from 'next/image';
+import dollarIcon from '../assets/icon-dollar.svg'; // make sure this file exists
 
 interface BillInputProps {
   bill: number | '';
@@ -8,16 +9,24 @@ interface BillInputProps {
 }
 
 export default function BillInput({ bill, setBill }: BillInputProps) {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setBill(value === '' ? '' : parseFloat(value));
+  };
+
   return (
     <div className="input-group">
       <label htmlFor="bill">Bill</label>
-      <input
-        id="bill"
-        type="number"
-        placeholder="0"
-        value={bill}
-        onChange={(e) => setBill(e.target.value === '' ? '' : parseFloat(e.target.value))}
-      />
+      <div className="input-with-icon">
+        <Image src={dollarIcon} alt="Dollar Icon" className="input-icon" />
+        <input
+          type="number"
+          id="bill"
+          placeholder="0"
+          value={bill}
+          onChange={handleChange}
+        />
+      </div>
     </div>
   );
 }
